9e6b2990c122cf40a5b97eb3b60ecf670b129b27
zub <ars1for1work@gmail.com>
2026-03-17 11:11:51 +0300

    Complete test framework

--- main.cpp
+++ main.cpp
@@ -1,5 +1,21 @@
-
+#include "top-it-vector.hpp"
+#include <iomanip>
+#include <iostream>
+bool testEmptyVector()
+{
+  return false;
+}
 int main()
 {
-
+  using test_t = std::pair< const char*, bool (*)() >;
+  test_t tests[] = {{"Empty vector", testEmptyVector}};
+  const size_t count = sizeof(tests) / sizeof(test_t);
+  std::cout << std::boolalpha;
+  bool pass = true;
+  for (size_t i = 0; i < count; ++i) {
+    bool res = tests[i].second();
+    std::cout << tests[i].first << ": " << res << "\n";
+    pass = pass && res;
+  }
+  std::cout << "RESULT: " << pass << '\n';
 }
--- top-it-vector.hpp
+++ top-it-vector.hpp
@@ -1,10 +1,26 @@
 #ifndef TOP_IT_VECTOR
 #define TOP_IT_VECTOR
 #include <cstddef>
-template <class T>
-struct Vector {
-  T* data;
-  size_t size,capacity;
-};
+namespace topit
+{
+  template < class T >
+  struct Vector
+  {
+    ~Vector();
+    Vector();
 
+  private:
+    T* data_;
+    size_t size_, capacity_;
+  };
+}
+template < class T >
+topit::Vector< T >::~Vector()
+{}
+template < class T >
+topit::Vector< T >::Vector():
+  data_(),
+  size_(),
+  capacity_()
+{}
 #endif
