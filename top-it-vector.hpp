a014c2859116246b2ccb1d69c67ca35116246607
zub <ars1for1work@gmail.com>
2026-03-24 09:48:38 +0300

    Add test and .at()

--- top-it-vector.hpp
+++ top-it-vector.hpp
@@ -16,6 +16,12 @@ namespace topit
     bool isEmpty() const noexcept;
     size_t getSize() const noexcept;
     size_t getCapacity() const noexcept;
+
+    T& operator[](size_t id) noexcept;
+    const T& operator[](size_t id) const noexcept;
+    T& at(size_t id);
+    const T& at(size_t id) const;
+
     void pushBack(const T& v);
     void popBack();
     void insert(size_t i, const T& v);
@@ -25,6 +31,22 @@ namespace topit
     T* data_;
     size_t size_, capacity_;
   };
+}
+template < class T >
+T& topit::Vector<T>::at(size_t id)
+{
+  if (id<getSize()) {
+    return data_[id];
+  }
+  throw std::range_error("bad id");
+}
+template < class T >
+const T& topit::Vector<T>::at(size_t id) const
+{
+  if (id<getSize()) {
+    return data_[id];
+  }
+  throw std::range_error("bad id");
 }
 template < class T >
 bool topit::Vector< T >::isEmpty() const noexcept
